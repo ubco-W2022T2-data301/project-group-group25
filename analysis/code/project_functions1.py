eaf0068ae50e5c1c58247a133c21d90147b121d4
PrabhtejDeol <122509639+PrabhtejDeol@users.noreply.github.com>
2023-03-23 18:14:55 -0700

    Add files via upload

--- analysis/code/project_functions1.py
+++ analysis/code/project_functions1.py
@@ -1,25 +1,6 @@
 import pandas as pd
 import numpy as np
 def load_and_process(url_or_path_to_csv_file):
+    # Add your data processing code here
+    pass
 
-    # Method Chain 1 (Load data and deal with missing data)
-
-    df1 = (
-          pd.read_csv(url_or_path_to_csv_file)
-          .rename(columns={"sex": "gender"})
-          .dropna(subset=['age', 'bmi', 'children', 'charges'])
-          .reset_index(drop=True)
-      )
-
-    # Method Chain 2 (Create new columns, drop others, and do processing)
-
-    df2 = (
-          df1
-          .assign(bmi_category = lambda x: np.where(x['bmi'] < 18.5, 'Underweight', np.where(x['bmi'] < 25, 'Normal', np.where(x['bmi'] < 30, 'Overweight', 'Obese'))))
-          .drop(columns=['region'])
-          .reset_index(drop=True)
-      )
-
-    # Make sure to return the latest dataframe
-
-    return df2
