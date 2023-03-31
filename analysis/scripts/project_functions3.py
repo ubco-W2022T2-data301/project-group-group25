33cbae707dddfce873e0a5922019886677557d56
nadiaricher <nadia.t.richer@gmail.com>
2023-03-31 11:29:52 -0700

    Fixed method chaining and load function in analysis3

--- analysis/scripts/project_functions3.py
+++ analysis/scripts/project_functions3.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np
+
 def load_and_process(path="../data/raw/insurance.csv"):
 
     # Method Chain 1 (Load data and deal with missing data)
@@ -7,21 +7,26 @@ def load_and_process(path="../data/raw/insurance.csv"):
     df1 = (
         pd.read_csv("../data/raw/insurance.csv")
         .drop_duplicates()
-        .dropna(subset = ["age","sex","bmi","children","smoker","region","charges"])
-        .reset_index(drop=True)
-        .drop(df[df["children"]<1].index)
     )
 
     # Method Chain 2 (Create new columns, drop others, and do processing)
 
     df2 = (
         df1
-        .assign(charge_per_child= (lambda x: x['charges'] / x['children'])).sort_values("charges",ascending=True).reset_index(drop=True)
         .rename(columns={"children":"number_of_children"})
+        .assign(charge_per_child= (lambda x: x['charges'] / x['number_of_children'])).sort_values("charges",ascending=True).reset_index(drop=True)
         .reset_index(drop=True)
-        
       )
+    df3=(
+        df2
+        .drop(df2[df2["number_of_children"]<1].index)
+        .dropna(subset = ["age","sex","bmi","number_of_children","smoker","region","charges"])
+        .reset_index(drop=True)
+    )
+        
+         
 
     # Make sure to return the latest dataframe
 
-    return df2 
+    return df3 
+
